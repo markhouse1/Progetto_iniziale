1d6af7390a637e0b91735e105df0ca39a1f4d207
markhouse1 <37371405+markhouse1@users.noreply.github.com>
2018-03-14 15:25:39 +0100

    Update index.java
    aggiunta dei commenti

--- index.java
+++ index.java
@@ -1,6 +1,11 @@
+ /* Questa è la prima applicazione java
+  * che va a stampare a video la frase "Prima Prova
+  * Questo è un commento multilinea
+  */
+
 public class HelloWorld {
    public static void main(String[] args) {
       // Stampa Prima prova
-      System.out.println("Prima");
+      System.out.println("Prima Prova");
    }
 }
